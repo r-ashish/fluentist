24c6d74b282b0f397364336893ef8a29a30ece5b
Ashish Ranjan <ashishranjan2912@gmail.com>
2019-07-21 01:55:26 +0800

    add option to go full screen

---
+++ fullscreen.js
@@ -0,0 +1,35 @@
+const elem = document.body;
+const fullScreenButton = document.getElementById("fullscreen-button");
+let isFullScreen = false;
+function toggleFullScreen() {
+  if (isFullScreen = !isFullScreen) {
+    openFullscreen();
+  } else {
+    closeFullscreen();
+  }
+}
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+  fullScreenButton.textContent = "fullscreen_exit";
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+  fullScreenButton.textContent = "fullscreen";
+}
